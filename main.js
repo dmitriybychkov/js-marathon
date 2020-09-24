f7c4c55b9343f6969611fda4f6a6efdce84bf8ef
dmitry <d.bychkov.95@mail.ru>
2020-09-24 19:55:18 +0300

    HomeWork 2

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-alert(1)
+const firstRow = 'мама мыла раму';
+const secondRow = 'собака друг человека';
+
+function getRow(firstRow, secondRow) {
+    let firstRowSum = 0,
+        secondRowSum = 0;
+    for (let i = 0; i < firstRow.length; i++) {
+        if (firstRow.charAt(i) === 'а') {
+            firstRowSum++;
+        }
+    }
+    for (let i = 0; i < secondRow.length; i++) {
+        if (secondRow.charAt(i) === 'а') {
+            secondRowSum++;
+        }
+    }
+    return firstRowSum > secondRowSum ? firstRow : secondRow;
+}
+console.log(getRow(firstRow, secondRow));
