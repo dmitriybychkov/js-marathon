8b028fa34630b720e24d7fc14f696214e5371f9a
dmitry <d.bychkov.95@mail.ru>
2020-09-27 11:53:13 +0300

    homework 3

--- main.js
+++ main.js
@@ -1,19 +1,59 @@
-const firstRow = 'мама мыла раму';
-const secondRow = 'собака друг человека';
+const $btn = document.getElementById('btn-kick');
 
-function getRow(firstRow, secondRow) {
-    let firstRowSum = 0,
-        secondRowSum = 0;
-    for (let i = 0; i < firstRow.length; i++) {
-        if (firstRow.charAt(i) === 'а') {
-            firstRowSum++;
-        }
-    }
-    for (let i = 0; i < secondRow.length; i++) {
-        if (secondRow.charAt(i) === 'а') {
-            secondRowSum++;
-        }
-    }
-    return firstRowSum > secondRowSum ? firstRow : secondRow;
+const character = {
+    name: 'Pikachu',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-character'),
+    elProgressbar: document.getElementById('progressbar-character')
 }
-console.log(getRow(firstRow, secondRow));
+
+const enemy = {
+    name: 'Charmander',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-enemy'),
+    elProgressbar: document.getElementById('progressbar-enemy')
+}
+
+$btn.addEventListener('click', function () {
+    console.log('Kick!');
+    changeHP(random(20), character);
+    changeHP(random(20), enemy);
+})
+
+function init() {
+    console.log('Start Game!');
+    renderHP(character);
+    renderHP(enemy);
+}
+
+function renderHPLife(person) {
+    person.elHP.innerText = person.damageHP + ' / ' + person.defaultHP;
+}
+
+function renderProgressbarHP(person) {
+    person.elProgressbar.style.width = person.damageHP + '%'
+}
+
+function renderHP(person) {
+    renderHPLife(person);
+    renderProgressbarHP(person);
+}
+
+function changeHP(count, person) {
+    if (person.damageHP < count) {
+        person.damageHP = 0;
+        alert(person.name + ' lose');
+        $btn.disabled = true;
+    } else {
+        person.damageHP -= count;
+    }
+    renderHP(person);
+}
+
+function random(num) {
+    return Math.ceil(Math.random() * num)
+}
+
+init();
