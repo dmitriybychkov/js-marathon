687a230266425cb587b4e4dba4054b7bfc4dac49
dmitry <d.bychkov.95@mail.ru>
2020-11-11 00:34:09 +0300

    'update'

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
-const firstRow = 'мама мыла раму';
-const secondRow = 'собака друг человека';
+// task 1
+
+const firstRow = prompt('First row:', 'мама мыла раму');
+const secondRow = prompt('Second row:', 'собака друг человека');
+const char = prompt('Введите букву');
 
 function getRow(firstRow, secondRow) {
-    let firstRowSum = 0,
-        secondRowSum = 0;
-    for (let i = 0; i < firstRow.length; i++) {
-        if (firstRow.charAt(i) === 'а') {
-            firstRowSum++;
-        }
-    }
-    for (let i = 0; i < secondRow.length; i++) {
-        if (secondRow.charAt(i) === 'а') {
-            secondRowSum++;
-        }
-    }
-    return firstRowSum > secondRowSum ? firstRow : secondRow;
+    return checkRow(firstRow) > checkRow(secondRow) ? firstRow :
+    checkRow(firstRow) < checkRow(secondRow) ? secondRow :
+    'There is no char like yours';
 }
-console.log(getRow(firstRow, secondRow));
+
+function checkRow(row) {
+    let countChar = 0;
+    for (let i = 0; i < row.length; i++) {
+        if (row.charAt(i) === char) {
+            countChar++;
+        }
+    }
+    return countChar;
+}
+
+alert(getRow(firstRow, secondRow));
